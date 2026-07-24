9607c5171e26b15e8230967725de6a0e4bec30e6
CfrancoChavez <franco.chvez@gmail.com>
2026-07-24 17:23:23 -0300

    Changes to resolve issues with navigation in contact page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,19 @@
 "use client"
+import { useEffect } from "react"
 import Hero from "./components/Hero"
 import Services from "./components/Services"
 import Projects from "./components/Projects"
 
 export default function Home() {
+  useEffect(() => {
+    if (window.location.hash) {
+      const id = window.location.hash
+      setTimeout(() => {
+        document.querySelector(id)?.scrollIntoView({ behavior: "smooth" })
+      }, 300)
+    }
+  }, [])
+
   return (
     <div className="overflow-x-hidden">
       <Hero />
